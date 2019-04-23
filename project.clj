8ea77133cffe9e52a4a3b497484b5427ce6659c2
Justin <justin@baal-software.com>
2019-04-22 18:05:01 -0600

    Add jdbc to project

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "The best backend ever"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 ]
   :main ^:skip-aot server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
